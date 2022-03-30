d9b6c5b69dcf7ee6a72128181a17d9bfecc56401
Naélio Freires <naeliosistemas@gmail.com>
2022-03-30 10:19:48 -0300

    separating unit tests and integration tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-  },
+  transform: { '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest' },
+  collectCoverageFrom: ['<rootDir>/components/**/*.js', '<rootDir>/pages/**/*.js'],
 };
