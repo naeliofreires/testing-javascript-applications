006ca1c629475d6c92c51f05fdaacaa6e970c7c3
Naélio Freires <naeliosistemas@gmail.com>
2022-04-07 12:48:49 -0300

    Revert "add test - actions"
    This reverts commit 0cb6f1bdce140ac6dd257dbac76f5001d140dbc1.

--- store/cart/cart-store.unit.spec.js
+++ store/cart/cart-store.unit.spec.js
@@ -1,56 +1,25 @@
 const { renderHook, act } = require('@testing-library/react-hooks');
 import { useCartStore } from './index';
-import { makeServer } from '../../miragejs/server';
 
 describe('Cart Store', () => {
-  let store;
-  let server;
-
-  beforeEach(() => {
-    server = makeServer({ environment: 'test' });
-    store = renderHook(() => useCartStore()).result;
-  });
-
-  afterEach(() => {
-    server.shutdown();
-
-    act(() => {
-      store.current.actions.reset();
-    });
-  });
-
   it('should return open equals false on initial state', async () => {
-    expect(store.current.state.open).toBe(false);
-  });
+    const { result } = renderHook(() => useCartStore());
 
-  it('should return an empty array for products on initial state', () => {
-    expect(Array.isArray(store.current.state.products)).toBe(true);
-    expect(store.current.state.products).toHaveLength(0);
-  });
-
-  it('should add 2 products to the list', async () => {
-    const products = server.createList('product', 2);
-
-    const { actions } = store.current;
-
-    for (const product of products) {
-      act(() => actions.add(product));
-    }
-
-    expect(store.current.state.products).toHaveLength(2);
+    expect(result.current.state.open).toBe(false);
   });
 
   it('should toggle open state', async () => {
+    const { result } = renderHook(() => useCartStore());
     const {
       actions: { toogle },
-    } = store.current;
+    } = result.current;
 
-    expect(store.current.state.open).toBe(false);
+    expect(result.current.state.open).toBe(false);
 
     act(() => toogle());
-    expect(store.current.state.open).toBe(true);
+    expect(result.current.state.open).toBe(true);
 
-    act(() => store.current.actions.toogle());
-    expect(store.current.state.open).toBe(false);
+    act(() => result.current.actions.toogle());
+    expect(result.current.state.open).toBe(false);
   });
 });
--- store/cart/index.js
+++ store/cart/index.js
@@ -1,25 +1,10 @@
 import create from 'zustand';
 
-const initialState = {
-  open: false,
-  products: [],
-};
-
 export const useCartStore = create((set) => ({
-  state: { ...initialState },
+  state: {
+    open: false,
+  },
   actions: {
-    reset: () =>
-      set(() => {
-        return { state: { ...initialState } };
-      }),
-    add: (product) =>
-      set((store) => ({
-        state: {
-          ...store.state,
-          open: true,
-          products: [...store.state.products, product],
-        },
-      })),
     toogle: () =>
       set((store) => {
         return { state: { open: !store.state.open } };
