054e28919c85b49551591907114a372fc70e1b5b
Naélio Freires <naeliosistemas@gmail.com>
2022-04-08 12:38:24 -0300

    fix: increase and decrease cart

--- components/cart-item.js
+++ components/cart-item.js
@@ -42,7 +42,7 @@ export default function CartItem({ product }) {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
 
@@ -51,7 +51,7 @@ export default function CartItem({ product }) {
             </span>
 
             <button
-              onClick={increase}
+              onClick={decrease}
               style={{ zIndex: 1 }}
               className="text-gray-500 focus:outline-none focus:text-gray-600"
             >
@@ -64,7 +64,7 @@ export default function CartItem({ product }) {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
           </div>
